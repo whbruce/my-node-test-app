2c13ebdd7b26e8eb1bd4fa6ea4dc531b1cff6ac2
Henry Bruce <henry.bruce@intel.com>
2017-01-19 11:42:51 -0800

    Updated to use library.
    Signed-off-by: Henry Bruce <henry.bruce@intel.com>

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+'use strict'
 var pkg = require('./package.json');
-console.log(pkg.name + " v" + pkg.version);
+var lib = require('my-node-test-lib');
+
+console.log("App: " + pkg.name + " v" + pkg.version);
+console.log("Lib: " + lib.getName() + " v" + lib.getVersion());
